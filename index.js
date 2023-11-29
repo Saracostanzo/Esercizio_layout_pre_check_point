b6075037bceb846ff5a5a595313cf4b497e9457c
costa <costanzosara96@gmail.com>
2023-11-29 09:35:47 +0100

    inserimento esercizio js

---
+++ index.js
@@ -0,0 +1,29 @@
+// In questo esercizio di codifica dovrai completare la funzione che trovi qui sotto.
+
+
+// Input 
+
+// L'input di questo metodo è un array contenente 10 numeri interi (numbersArray).
+
+// Output
+// restituisce un numero intero (contatore).
+
+
+// La funzione deve essere implementata come segue:
+
+// "Dato un array di numeri interi positivi e negativi come input, conta le occorrenze del numero 0 e restituiscilo in output".
+
+// Esempi:
+
+// Input: [9,6,3,0,1,2,3,4,5,0] Output: 2
+
+// Input: [1,1,0,0,1,1,0,0,1,1] Output: 4
+
+// Input: [-1,1,-2,2,-3,3,0,0,0,0] Output: 4
+
+
+function counterZero( numbersArray ) {
+
+    //Insert your code here 
+    
+}
